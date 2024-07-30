838d2d1935299c78608a605dfb652468f223e627
Garren Tan <garrentmh@gmail.com>
2024-07-30 09:39:19 +0800

    change into not using config2.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Replace with your actual email
-const email = config.email;
-const resumeLink = config.resumeLink;
+const email = `darrelltmh@gmail.com`;
+const resumeLink = `https://drive.google.com/file/d/1yTc0YtKKai4-CKSjELQbsVdxRh2wnpoh/view?usp=drive_link`;
 document.getElementById("email-contact").innerText = email;
 document.getElementById("footer-email").innerText = email;
 
